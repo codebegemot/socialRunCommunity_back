40435d037a4a1d116005eb9e4a29b68250a3c21f
codebegemot <SergeevDA@stud.kai.ru>
2024-10-25 23:47:52 +0300

    Добавил .gitignore и удалил лишние файлы из индекса

--- socialRunCommunity/Models/Event.cs
+++
@@ -1,9 +0,0 @@
-public class Event 
-{
-    public int Id { get; set;}
-    public string Title { get; set;}
-    public string Description { get; set;}
-    public DateTime EventDate { get; set;}
-    public int MaxParticipants { get; set;}
-    public ICollection<EventParticipant> EventParticipants{ get; set;}
-}
